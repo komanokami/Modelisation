6ce8c752a5e1dac292c5e523ab8f739d9b03141e
Kaminuri <matt-picard@hotmail.fr>
2015-02-04 18:09:51 +0100

    Constants

---
+++ Modelisation/src/publics/Const.java
@@ -0,0 +1,13 @@
+package publics;
+
+import java.util.ArrayList;
+
+import entity.Face;
+import entity.Point;
+import entity.Segment;
+
+public class Const {
+	public static ArrayList<Face> faces = new ArrayList<>();
+	public static ArrayList<Point> points = new ArrayList<>();
+	public static ArrayList<Segment> segments = new ArrayList<>();
+}
