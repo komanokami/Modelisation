24241bc07e67dee55b8399736a8e05f86287d46b
Kaminuri <matt-picard@hotmail.fr>
2015-02-04 19:04:35 +0100

    TestLoad

--- Modelisation/src/loader/FileLoader.java
+++ Modelisation/src/loader/FileLoader.java
@@ -1,10 +1,21 @@
 package loader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import publics.Const;
 
 public class FileLoader {
 	public FileLoader(){
 		clearLists();
+		String sta;
+		try(BufferedReader br = new BufferedReader(new FileReader("/Modelisation/src/models/cone.gts"))){
+			sta = br.readLine();
+			System.out.println(sta);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 	private void clearLists() {
@@ -12,6 +23,9 @@ public class FileLoader {
 		Const.points.clear();
 		Const.segments.clear();
 		
+	}
+	public static void main(String args[]){
+		FileLoader f = new FileLoader();
 	}
 	
 }
