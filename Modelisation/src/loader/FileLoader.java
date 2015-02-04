79b873108410863a71cd19ce2ec95b1a56a5fd7b
Kaminuri <matt-picard@hotmail.fr>
2015-02-04 19:16:16 +0100

    Début codage Loader! C'pas mal :D

--- Modelisation/src/loader/FileLoader.java
+++ Modelisation/src/loader/FileLoader.java
@@ -7,15 +7,23 @@ import java.io.IOException;
 import publics.Const;
 
 public class FileLoader {
+	private int nbPoint, nbSeg, nbFace;
 	public FileLoader(){
 		clearLists();
 		String sta;
-		try(BufferedReader br = new BufferedReader(new FileReader("/Modelisation/src/models/cone.gts"))){
+		try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Mathias\\Documents\\GitHub\\Modelisation\\Modelisation\\src\\models\\cone.gts"))){
 			sta = br.readLine();
-			System.out.println(sta);
+			initNbs(sta);
 		}catch(IOException e){
 			e.printStackTrace();
 		}
+	}
+
+	private void initNbs(String sta) {
+		String words[] = sta.split(" ");
+		nbPoint = Integer.parseInt(words[0]);
+		nbSeg = Integer.parseInt(words[1]);
+		nbFace = Integer.parseInt(words[2]);
 	}
 
 	private void clearLists() {
